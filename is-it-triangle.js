5f2450ebea22c47664df891b64ed765e648f133e
Abhishek <67749693+abhishake21@users.noreply.github.com>
2022-06-10 09:34:53 +0530

    Added Output message functions

--- is-it-triangle.js
+++ is-it-triangle.js
@@ -2,9 +2,11 @@ const angles = document.querySelectorAll('.angles');
 const checkBtn = document.querySelector('.check-btn');
 
 const message = document.querySelector('#output-message');
-const gif = document.querySelector('.gif');
+const triangleGif = document.querySelector('.triangle-gif');
+const notTriangleGif = document.querySelector('.not-triangle-gif');
 
-gif.style.display = "none";
+triangleGif.style.display = "none";
+notTriangleGif.style.display = "none";
 message.style.display = "none";
 
 checkBtn.addEventListener('click', checkTriangle);
@@ -14,26 +16,33 @@ function checkTriangle() {
 
     if (angles[0].value && angles[1].value && angles[2].value) {
         if (sum === 180) {
-            message.innerHTML = 'Yes, Its a <span>Triangle</span>';
-            message.style.display = "block";
-            gif.src = './Images/triangle.gif';
-            gif.alt = 'Glowing triangle';
-            gif.style.display = "block";
+            triangleMsgOutput();
         } else {
-            message.innerHTML = 'No, Its <span>Not a Triangle</span>';
-            message.style.display = "block";
-            gif.src = './Images/not-triangle.gif';
-            gif.alt = 'Weird shape morphing';
-            gif.style.display = "block";
+            notTriangleMsgOutput();
         }
     } else {
         message.innerText = 'Enter all fields.';
         message.style.display = "block";
-        gif.style.display = "none";
+        triangleGif.style.display = "none";
+        notTriangleGif.style.display = "none";
     }
 }
 
 function sumOfAngels(angle1, angle2, angle3) {
     const sum = Number(angle1) + Number(angle2) + Number(angle3);
     return sum;
+}
+
+function triangleMsgOutput() {
+    message.innerHTML = 'Yes, Its a <span>Triangle</span>';
+    message.style.display = "block";
+    notTriangleGif.style.display = "none";
+    triangleGif.style.display = "block";
+}
+
+function notTriangleMsgOutput() {
+    message.innerHTML = 'No, Its <span>Not a Triangle</span>';
+    message.style.display = "block";
+    triangleGif.style.display = "none";
+    notTriangleGif.style.display = "block";
 }
